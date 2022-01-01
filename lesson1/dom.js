33b167e76e07ba302381485d1345048ed0c7d2c8
hungtran <immorteltmh@gmail.com>
2022-01-02 06:49:42 +0700

    update 2/1/2022

--- lesson1/dom.js
+++ lesson1/dom.js
@@ -1,2 +1,7 @@
-var headingNode = document.querySelector('.heading')
-console.log([headingNodes])
+// var box1Element = document.getElementsByClassName('heading:first-child')
+// console.log([box1Element])
+
+var h1Element = document.getElementsByTagName('h1')
+var h2Element = document.getElementsByTagName('h2')
+var h3Element = document.getRootNode
+console.log([h2Element])
